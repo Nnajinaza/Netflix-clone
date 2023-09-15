ba004ce7c9c991230fdf54755fccc1f7586fb037
Nnajinaza <nnajinaza@gmail.com>
2023-09-16 00:44:29 +0100

    ui created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from "react";
+import Nav from "../components/Nav";
+import Banner from "../components/Banner";
+import { RiArrowRightSLine } from "react-icons/ri";
 
 const Home = () => {
   return (
-    <div>
-      <Nav />
-      
-    </div>
-  )
-}
+    <>
+      <section className="banner-section">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col12">
+              <Banner />
+              <Nav />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="featured-first-row">
+        <div className="featured-section">
+          <div className="featured-movie">
+            <h4 className="featured">Featured Movie</h4>
+          </div>
+          <div className="see-more-option">
+            <h5 className="see-more">See more</h5>
+            <RiArrowRightSLine color="#BE123C" />
+          </div>
+        </div>
+      </section>
+      <section className="featured-movies">
+        <div className="featured-card">
+          <img src="images/stranger-things.jpg" alt="stranger things" />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
